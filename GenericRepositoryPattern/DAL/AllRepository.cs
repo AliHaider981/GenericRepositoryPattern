a53bdf84a1cf85e9b08a65455796c30f5c2a7c4c
ALI <ALI@DESKTOP-4M3SNQD>
2020-02-24 20:31:05 +0500

    Create_Interface

---
+++ GenericRepositoryPattern/DAL/AllRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GenericRepositoryPattern.DAL
+{
+    public class AllRepository<T> : _IAllRepository<T> where T : class
+    {
+    }
+}
---
+++ GenericRepositoryPattern/DAL/_IAllRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRepositoryPattern.DAL
+{
+    interface _IAllRepository<T> where T : class
+    {
+        IEnumerable<T> GetModel();
+        T GetModelById(int id);
+        void InsertModel(T model);
+        void DeleteModel(int id);
+        void UpdateModel(T model);
+        void Save();
+    }
+}
