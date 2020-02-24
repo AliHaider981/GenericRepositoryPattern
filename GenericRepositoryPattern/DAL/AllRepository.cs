e4af966cd4e67d8d000f9356de255c65b0e36184
ALI <ALI@DESKTOP-4M3SNQD>
2020-02-24 23:50:48 +0500

    Create-ALLRepository

--- GenericRepositoryPattern/DAL/AllRepository.cs
+++ GenericRepositoryPattern/DAL/AllRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GenericRepositoryPattern.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,44 @@ namespace GenericRepositoryPattern.DAL
 {
     public class AllRepository<T> : _IAllRepository<T> where T : class
     {
+        private DataContext _context;
+        private DbSet<T> dbEntity;
+
+        public AllRepository()
+        {
+            _context = new DataContext();
+            dbEntity = _context.Set<T>();
+        }
+
+        public void DeleteModel(int id)
+        {
+            T Model = dbEntity.Find(id);
+            dbEntity.Remove(Model);
+        }
+
+        public IEnumerable<T> GetModel()
+        {
+            return dbEntity.ToList();
+        }
+
+        public T GetModelById(int id)
+        {
+            return dbEntity.Find(id);
+        }
+
+        public void InsertModel(T model)
+        {
+            dbEntity.Add(model);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void UpdateModel(T model)
+        {
+            _context.Entry(model).State = EntityState.Modified; 
+        }
     }
 }
